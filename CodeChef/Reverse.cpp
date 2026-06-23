09bfe6113ba9c3d0cf7d30b815f85fc532226e2c
Abhishek Kumar <47770860+AbhishekKumar-cse@users.noreply.github.com>
2026-06-23 10:31:25 +0000

    Add Basics : Reverse Number

---
+++ CodeChef/Reverse.cpp
@@ -0,0 +1,28 @@
+//Question Link
+//https://www.codechef.com/LRNDSA01/problems/FLOW007
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int rev = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n /= 10;
+        }
+
+        cout << rev << endl;
+    }
+
+    return 0;
+}
