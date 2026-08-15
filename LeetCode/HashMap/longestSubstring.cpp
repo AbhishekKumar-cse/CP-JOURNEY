b162ea7a286b369db371db9dacae1a30861cb2f0
Abhishek Kumar <47770860+AbhishekKumar-cse@users.noreply.github.com>
2026-08-15 18:34:53 +0000

    add : longest Substring in Hash map

---
+++ LeetCode/HashMap/longestSubstring.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int> mp;
+        int ans=0;
+        int left=0;
+
+        for(int i=0; i<s.size(); i++){
+            if(mp.find(s[i]) != mp.end()){
+                left = max(left, mp[s[i]]+1);
+            }
+
+            mp[s[i]] =i;
+            ans = max(ans , i - left +1);
+        }
+
+        return ans;
+
+    }
+};
